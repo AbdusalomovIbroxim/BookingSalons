73a98457d15c19a6a4965c6a35f9974eb4d2c66b
Ibroxim <ibrohimlololowka@gmail.com>
2025-05-31 01:50:34 +0500

    update: allowed hostes for all hosts

--- BookingSalons/settings.py
+++ BookingSalons/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-=fl)j^y&&(hpp4l(gjq4dvt)o0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
