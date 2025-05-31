ab23049f57ff4495e51bc58e54e89a09901ac7ba
Ibroxim <ibrohimlololowka@gmail.com>
2025-05-31 12:24:34 +0500

    update: update configs for prod

--- BookingSalons/settings.py
+++ BookingSalons/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-=fl)j^y&&(hpp4l(gjq4dvt)o0d-a*z(#klt$9-3g)j%)33(^y')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -129,10 +129,10 @@ SWAGGER_SETTINGS = {
     ],
 
     # настройки чтобы свагер работал и в проде
-    'VALIDATOR_URL': None,
-    'PERSIST_AUTH': True,
-    'REFETCH_SCHEMA_WITH_AUTH': True,
-    'REFETCH_SCHEMA_ON_LOGOUT': True,
+    # 'VALIDATOR_URL': None,
+    # 'PERSIST_AUTH': True,
+    # 'REFETCH_SCHEMA_WITH_AUTH': True,
+    # 'REFETCH_SCHEMA_ON_LOGOUT': True,
 }
 
 # Password validation
@@ -188,20 +188,20 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Настройки для работы в production
-if not DEBUG:
-    # Настройки безопасности
-    SECURE_SSL_REDIRECT = True
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
-    SECURE_BROWSER_XSS_FILTER = True
-    SECURE_CONTENT_TYPE_NOSNIFF = True
-    X_FRAME_OPTIONS = 'DENY'
-    SECURE_HSTS_SECONDS = 31536000  # 1 год
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    SECURE_HSTS_PRELOAD = True
+# if not DEBUG:
+#     # Настройки безопасности
+#     SECURE_SSL_REDIRECT = True
+#     SESSION_COOKIE_SECURE = True
+#     CSRF_COOKIE_SECURE = True
+#     SECURE_BROWSER_XSS_FILTER = True
+#     SECURE_CONTENT_TYPE_NOSNIFF = True
+#     X_FRAME_OPTIONS = 'DENY'
+#     SECURE_HSTS_SECONDS = 31536000  # 1 год
+#     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+#     SECURE_HSTS_PRELOAD = True
 
-    # Настройки статических файлов
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-    WHITENOISE_USE_FINDERS = True
-    WHITENOISE_MANIFEST_STRICT = False
-    WHITENOISE_ALLOW_ALL_ORIGINS = True
+#     # Настройки статических файлов
+#     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#     WHITENOISE_USE_FINDERS = True
+#     WHITENOISE_MANIFEST_STRICT = False
+#     WHITENOISE_ALLOW_ALL_ORIGINS = True
